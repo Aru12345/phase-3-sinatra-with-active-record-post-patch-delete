125755f31c557fce8b6d6a44cfe3712b3c5c6e2e
Arundhati <arundhatiraicar12@gmail.com>
2022-03-30 01:09:15 +0530

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,26 @@ require 'pry'
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
+  get '/games' do
+    games=Game.all 
+    games.to_json
+
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+
+    game.to_json(include: {reviews: {include: :user}})
+
+  end
+
   delete '/reviews/:id' do
     # find the review using the ID
+    review = Review.find(params[:id])
     # delete the review
+    review.destroy
     # send a response with the deleted review as JSON
+    review.to_json
   end
   
     post '/reviews' do
